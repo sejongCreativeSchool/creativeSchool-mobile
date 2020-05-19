01da6229299ccd7f88593104725a15bf2b304417
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-05-19 20:22:36 +0900

    [set]set edittext imeOptions(enter key action)

--- Booreum/app/src/main/java/com/booreum/booreum/signup/SignUp.java
+++ Booreum/app/src/main/java/com/booreum/booreum/signup/SignUp.java
@@ -1,14 +1,14 @@
 package com.booreum.booreum.signup;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import com.booreum.booreum.CustomAppCompatForToolbar;
-import com.booreum.booreum.MyToolBar;
 import com.booreum.booreum.R;
 
 public class SignUp extends CustomAppCompatForToolbar {
@@ -27,5 +27,24 @@ public class SignUp extends CustomAppCompatForToolbar {
     protected void linkToolbar() {
         toolbar = findViewById(R.id.signup_toolbar);
         setSupportActionBar(toolbar.getToolbar());
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev)
+    {
+        View cView = getCurrentFocus();
+        if(cView != null)
+        {
+            Rect rect = new Rect();
+            cView.getGlobalVisibleRect(rect);
+            int x = (int) ev.getX(), y = (int) ev.getY();
+            if (!rect.contains(x, y)) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                if (imm != null)
+                    imm.hideSoftInputFromWindow(cView.getWindowToken(), 0);
+                cView.clearFocus();
+            }
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
