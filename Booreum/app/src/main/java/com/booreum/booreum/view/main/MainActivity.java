363b8d6bbb7461b2f99b01d91f5f503eb57104ab
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-05-26 20:21:43 +0900

    [add]add fragment, adapter

--- Booreum/app/src/main/java/com/booreum/booreum/view/main/MainActivity.java
+++ Booreum/app/src/main/java/com/booreum/booreum/view/main/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.booreum.booreum.CustomToolbar.CustomAppCompatForToolbar;
@@ -26,8 +29,18 @@ public class MainActivity extends CustomAppCompatForToolbar implements I_MainVie
         setContentView(R.layout.activity_main);
 
         tabLayout = (TabLayout)findViewById(R.id.main_tapLayout);
+        tabLayout.addTab(tabLayout.newTab().setCustomView(createTabView(R.drawable.ic_launcher_background)));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(createTabView(R.drawable.ic_launcher_background)));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(createTabView(R.drawable.ic_launcher_background)));
+
     }
 
+    private View createTabView(int resId) {
+        View tabView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.custom_tab, null);
+        ImageView imageView = (ImageView)tabView.findViewById(R.id.custom_tab_icon);
+        imageView.setImageResource(resId);
+        return tabView;
+    }
 
     @Override
     protected void linkToolbar() {
